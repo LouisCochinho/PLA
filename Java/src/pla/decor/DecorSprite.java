73d972e8baacc57c9ab5cd51e849f1df84153786
Antoine Gambro <antoine.gambro@outlook.com>
2016-06-10 16:06:08 +0200

    sprite

--- Java/src/pla/decor/DecorSprite.java
+++ Java/src/pla/decor/DecorSprite.java
@@ -10,19 +10,19 @@ package pla.decor;
  * @author antoi
  */
 public enum DecorSprite {
-    BATIMENT_NEUTRE(0, 7),
-    BOMBE_EAU(0, 5),
-    BOMBE_PEINTURE(0, 4),
-    BOUCHE_EGOUT(0, 9),
-    GENDARMERIE(0, 3),
+    BATIMENT_NEUTRE(0, 11),
+    BOMBE_EAU(0, 7),
+    BOMBE_PEINTURE(0, 6),
+    BOUCHE_EGOUT(0, 4),
+    GENDARMERIE(0, 1),
     HELICO(0, 1),
     MUR(0, 0),
-    MURET(0, 1),
-    SKATEPARK(0, 11),
-    SOL_AMI(0, 8),
+    MURET(0, 5),
+    SKATEPARK(0, 2),
+    SOL_AMI(0, 3),
     SOL_ENNEMI(0, 10),
-    SOL_NORMAL(0, 6),
-    VELO(0, 2);
+    SOL_NORMAL(0, 9),
+    VELO(0, 8);
     
     private final int x;
     private final int y;

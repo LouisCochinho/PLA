828a3b8c67e148176a97cd9787bf5c7c53fb95b1
Antoine Gambro <antoine.gambro@outlook.com>
2016-06-15 10:13:02 +0200

    les actions sont là !!!

--- Java/src/pla/Jeu.java
+++ Java/src/pla/Jeu.java
@@ -214,6 +214,8 @@ public class Jeu extends BasicGameState {
 		if (!indexPossibles.isEmpty()) {
 			// Prendre un index au hasard dans la liste
 			indexChoisi = indexPossibles.get(r.nextInt(indexPossibles.size()));
+                        
+                        p.getAutomate().getTabActionTransition()[indexChoisi][etatCourantId].executer(p, map.getCaseFromCoord((int)p.getX(), (int)p.getY()), this, 0);
 			// System.out.println("index choisi : "+indexChoisi);
 			// System.out.println("etat suivant :
 			// "+p.getAutomate().getTabEtatSuivant()[indexChoisi][etatCourantId].getId());
@@ -222,7 +224,6 @@ public class Jeu extends BasicGameState {
 			/*System.out.println("index choisi : " + indexChoisi);
 			System.out.println(
 					"etat suivant : " + p.getAutomate().getTabEtatSuivant()[indexChoisi][etatCourantId].getId());*/
-			p.setEtatCourant(p.getAutomate().getTabEtatSuivant()[indexChoisi][etatCourantId]);
 
 		} else {
 			p.setEtatCourant(p.getAutomate().getEtatInitial());

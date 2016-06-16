9ffc01e8328151b7b963595c746dab048156ceaf
Damien Siest <damiensiest-etudes@outlook.fr>
2016-06-16 16:41:25 +0200

    Conditon combinée

--- Ocaml/Automates/automateCondComb.ml
+++ Ocaml/Automates/automateCondComb.ml
@@ -9,11 +9,75 @@ let (list_etat:etat list)=
   ];;
 
 let autTest =
-  [ (0, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),N)),PeindreNeutre(Ami),3);
-    (0, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),N)),PeindreNeutre(Ami),3);
-    (0, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),E)),PeindreNeutre(Ami),2);
-    (0, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),E)),PeindreNeutre(Ami),2);
-    (0, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),O)),PeindreNeutre(Ami),1);
-    (0, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),O)),PeindreNeutre(Ami),1);
-    (0, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),S)),PeindreNeutre(Ami),0);
-    (0, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),S)),PeindreNeutre(Ami),0);]
+  [ (0, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),N)),PeindreNeutre,3);
+    (0, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),N)),PeindreNeutre,3);
+    (0, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),E)),PeindreNeutre,2);
+    (0, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),E)),PeindreNeutre,2);
+    (0, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),O)),PeindreNeutre,1);
+    (0, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),O)),PeindreNeutre,1);
+    (0, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),S)),PeindreNeutre,0);
+    (0, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),S)),PeindreNeutre,0);
+    
+    (1, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),N)),PeindreNeutre,3);
+    (1, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),N)),PeindreNeutre,3);
+    (1, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),E)),PeindreNeutre,2);
+    (1, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),E)),PeindreNeutre,2);
+    (1, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),O)),PeindreNeutre,1);
+    (1, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),O)),PeindreNeutre,1);
+    (1, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),S)),PeindreNeutre,0);
+    (1, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),S)),PeindreNeutre,0);
+
+    (2, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),N)),PeindreNeutre,3);
+    (2, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),N)),PeindreNeutre,3);
+    (2, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),E)),PeindreNeutre,2);
+    (2, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),E)),PeindreNeutre,2);
+    (2, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),O)),PeindreNeutre,1);
+    (2, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),O)),PeindreNeutre,1);
+    (2, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),S)),PeindreNeutre,0);
+    (2, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),S)),PeindreNeutre,0);
+
+    (3, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),N)),PeindreNeutre,3);
+    (3, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),N)),PeindreNeutre,3);
+    (3, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),E)),PeindreNeutre,2);
+    (3, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),E)),PeindreNeutre,2);
+    (3, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),O)),PeindreNeutre,1);
+    (3, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),O)),PeindreNeutre,1);
+    (3, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),S)),PeindreNeutre,0);
+    (3, Et(Decor(Sol(Ennemi),C),Decor(Sol(Ennemi),S)),PeindreNeutre,0);
+
+    (0, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),N)),PeindreNeutre,3);
+    (0, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),N)),PeindreNeutre,3);
+    (0, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),E)),PeindreNeutre,2);
+    (0, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),E)),PeindreNeutre,2);
+    (0, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),O)),PeindreNeutre,1);
+    (0, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),O)),PeindreNeutre,1);
+    (0, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),S)),PeindreNeutre,0);
+    (0, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),S)),PeindreNeutre,0);
+    
+    (1, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),N)),PeindreNeutre,3);
+    (1, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),N)),PeindreNeutre,3);
+    (1, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),E)),PeindreNeutre,2);
+    (1, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),E)),PeindreNeutre,2);
+    (1, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),O)),PeindreNeutre,1);
+    (1, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),O)),PeindreNeutre,1);
+    (1, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),S)),PeindreNeutre,0);
+    (1, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),S)),PeindreNeutre,0);
+
+    (2, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),N)),PeindreNeutre,3);
+    (2, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),N)),PeindreNeutre,3);
+    (2, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),E)),PeindreNeutre,2);
+    (2, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),E)),PeindreNeutre,2);
+    (2, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),O)),PeindreNeutre,1);
+    (2, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),O)),PeindreNeutre,1);
+    (2, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),S)),PeindreNeutre,0);
+    (2, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),S)),PeindreNeutre,0);
+
+    (3, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),N)),PeindreNeutre,3);
+    (3, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),N)),PeindreNeutre,3);
+    (3, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),E)),PeindreNeutre,2);
+    (3, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),E)),PeindreNeutre,2);
+    (3, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),O)),PeindreNeutre,1);
+    (3, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),O)),PeindreNeutre,1);
+    (3, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),S)),PeindreNeutre,0);
+    (3, Et(Decor(Sol(Ami),C),Decor(Sol(Ami),S)),PeindreNeutre,0);
+]

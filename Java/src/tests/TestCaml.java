75a2223ddc7daa483eaf8b9be89ae2accfc408d2
Antoine Gambro <antoine.gambro@outlook.com>
2016-06-16 09:33:29 +0200

    testcaml

--- Java/src/tests/TestCaml.java
+++ Java/src/tests/TestCaml.java
@@ -10,18 +10,23 @@ public class TestCaml {
 	 * @param args
 	 * @throws IOException 
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		ProcessBuilder pb = new ProcessBuilder("make");
-		pb.directory(new File("../Ocaml/"));
-		File log = new File("log");
-		pb.redirectErrorStream(true);
-		pb.redirectOutput(Redirect.appendTo(log));
-		Process p = pb.start();
-		assert pb.redirectInput() == Redirect.PIPE;
-		assert pb.redirectOutput().file() == log;
-		assert p.getInputStream().read() == -1;
+                try {
+                    ProcessBuilder pb = new ProcessBuilder("make");
+                    pb.directory(new File("../Ocaml/"));
+                    File log = new File("log");
+                    pb.redirectErrorStream(true);
+                    pb.redirectOutput(Redirect.appendTo(log));
+                    Process p = pb.start();
+                    assert pb.redirectInput() == Redirect.PIPE;
+                    assert pb.redirectOutput().file() == log;
+                    assert p.getInputStream().read() == -1;
+                }
+                catch(IOException e) {
+                    System.out.println(e.getMessage());
+                }
 	}
 
 }
